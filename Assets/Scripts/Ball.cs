8c5dc0985e7f715cd6eb32f011266dde037f2c59
Markus Sorensen <markus5a@hotmail.com>
2017-04-21 18:43:43 +0200

    2d platformer example

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float rotationSpeed = 1;
+    public float jumpHeight = 1;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void FixedUpdate()
+    {
+        //Rotation
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rb.AddTorque(-rotation);
+        //rb.AddRelativeTorque(Vector3.back * rotation);
+
+        //Jump
+        if (Input.GetKey("space"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 targetPos = target.transform.position;
+        Vector3 position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+        transform.position = position;
+	}
+}
