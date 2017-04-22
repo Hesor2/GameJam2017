754ad5f74d68a7b00ef7a119d78b955150f0c2c5
Marcus Melgaard <mmjworkgg@gmail.com>
2017-04-22 12:50:37 +0200

    minor things

---
+++ Assets/Scripts/LugNutAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LugNutAI : MonoBehaviour {
+
+    GameObject player;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        print(player.transform.position);
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
